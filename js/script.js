49dac5a29bfc4db200a22344804fad65be13ab02
Mellisah-lisah <aliceochieng25@gmail.com>
2020-03-02 11:06:17 +0300

    added changes in html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function getSizeCost() {
-  var selectedSize = document.getElementById("size").nodeValue;
+  var selectedSize = document.getElementById("size").value;
   return parseInt(selectedSize);
 }
 
@@ -7,68 +7,77 @@ function getCrustCost() {
   var selectedCrust = document.getElementById("crust").value;
   return parseInt(selectedCrust);
 }
-
 function getNumber() {
-  var selectedNumber = document.getElementById("numberofPizza").value;
-  return parseInt("selectedNumber");
+  var selectedNumber = document.getElementById("numberofpizza").value;
+  return parseInt(selectedNumber);
 }
-
 function mushrooms() {
   var mushroom = 0;
-  var addMushroom = document.getElementById("toppingone");
+  var addMushroom = document.getElementById("topping5");
   if (addMushroom.checked === true) {
-    mushroom = 200;
+      mushroom = 150;
   }
   return parseInt(mushroom);
 }
-function mozarellaCheese () {
+
+function sausages() {
+  var sausage = 0;
+  var addsausage = document.getElementById("topping1");
+  if (addsausage.checked === true) {
+      sausage = 140;
+  }
+  return parseInt(sausage);
+}
+function onions() {
+  var onion = 0;
+  var addonions = document.getElementById("topping2");
+  if (addMushroom.checked === true) {
+      onion = 130;
+  }
+  return parseInt(onion);
+}
+function tikkaSaucee() {
+  var tikkaSauce = 0;
+  var addtikkaSauce = document.getElementById("topping3");
+  if (addtikkaSauce.checked === true) {
+      tikkaSauce = 120;
+  }
+  return parseInt(tikkaSauce);
+}
+function mozarellaCheese() {
   var cheese = 0;
-  var addmozarellaCheese = document.getElementById("tropping");
+  var addmozarellaCheese = document.getElementById("topping4");
   if (addmozarellaCheese.checked === true) {
-    cheese = 150;
+      cheese = 110;
   }
   return parseInt(cheese);
 }
 
-function onions() {
-  var onion = 0;
-  var addonions = document.getElementById("troppingone");
-  if (addonions.checked ===true) {
-  onion = 130;
+function calctotalPrice(e) {
+  event.preventDefault();
+  var totalPrice = (getSizeCost() + getCrustCost() + mushrooms())  * (getNumber());
+  
+      console.log(totalPrice);
+  alert("Your order of " + getNumber() + " pizzas has been processed.Your total amount payable is " + totalPrice +"." )
+
 }
-return parseInt(onion);
-}
-function tikkaSauce() {
- var tikkaSauce = 0;
- var addtikkaSauce = document.getElementById("toppingone");
- if ("addtikkaSauce.checked === true") {
-   tikkaSauce = 120;
- }
- return parseInt(tikkaSauce); 
-}
-function sausage() {
-  var sausage = 0;
-  var addsausage = document.getElementById("troppingone");
-  if (addsausage.checked === true) {
-    sausage = 110;
-  }
-  return parseInt(sausage);
-}
-$("button#complete-order").click(function() {
-  $(".delvery").show();
+
+
+
+// Delivery form inputs and alerts
+
+
+
+$(document).ready(function () {
+  $("#delivery").submit(function () {
+
+      var name = $("input#name").val();
+      var number = $("input#number").val();
+      var location = $("input#location").val();
+
+      alert("Hello " + name + ". Your order has been successfuly received and will be delivered to " +  location + " within one hour.The delivery will cost ksh 180/= Thank you for chosing the pizzeria.");
+      // $(this).get(0).reset();
+      //  event.preventDefault();
+  });
+  
 });
-$("button#yes").click(function() {
-  $(".delivery").hide();
-  $(".location").show();
-});
-$("button#location-send").click(function() {
-  $(".location").hide();
-  var location = $("input#yourlocation").val();
-  $("#text").text(" Please  Enter Your Location" + location + "");
-  $(".pyment").show();
-});
-$("input#yourLocation").val("");
-$("button#no").click(function() {
-  $(".delivery").hide();
-  $(".noDelivery").show();
-})
